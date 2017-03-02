8b7bf72292bdb6bbcf8979bd41882ba736603232
chfleming <flemingc@si.edu>
2017-03-02 14:46:08 -0500

    writeRaster

---
+++ R/export.R
@@ -0,0 +1,137 @@
+# R compilation is complaining about using raster S4 methods on my S3 UD class
+methods::setOldClass("UD")
+
+################################
+# create a raster of the ML akde
+raster.UD <- function(x,DF="CDF",...)
+{
+  UD <- x
+  
+  dx <- UD$dr[1]
+  dy <- UD$dr[2]
+  
+  xmn <- UD$r$x[1]-dx/2
+  xmx <- last(UD$r$x)+dx/2
+  
+  ymn <- UD$r$y[1]-dy/2
+  ymx <- last(UD$r$y)+dy/2
+  
+  # probability mass for the cells
+  if(DF=="PMF")
+  {
+    DF <- "PDF"
+    UD[[DF]] <- UD[[DF]] * prod(UD$dr)
+  }
+  
+  Raster <- raster::raster(t(UD[[DF]][,dim(UD[[DF]])[2]:1]),xmn=xmn,xmx=xmx,ymn=ymn,ymx=ymx,crs=attr(UD,"info")$projection)
+  
+  return(Raster)
+}
+methods::setMethod("raster",signature(x="UD"), function(x,DF="CDF",...) raster.UD(x,DF=DF,...))
+
+
+##########################
+writeRaster.UD <- function(x,filename,format,DF="CDF",...)
+{
+  x <- raster(x,DF="CDF") # ... here seems much less useful
+  writeRaster(x,filename,format,...)
+}
+methods::setMethod("writeRaster",signature(x="UD",filename="character"), function(x,filename,format,DF="CDF",...) writeRaster.UD(x,filename,format,DF=DF,...))
+
+
+################
+# Is contour A inside contour B
+inside <- function(A,B)
+{
+  result <- mode(sp::point.in.polygon(A$x,A$y,B$x,B$y))
+  if(1<=result && result<=2) { return(1) } else { return(0) }
+}
+
+
+##############
+SpatialPolygonsDataFrame.UD <- function(object,level.UD=0.95,level=0.95,...)
+{
+  UD <- object
+  
+  P <- CI.UD(UD,level.UD,level,P=TRUE)
+  NAMES <- names(P)
+  
+  ID <- paste(UD@info$identity," ",round(100*level.UD),"% ",NAMES,sep="")
+  
+  polygons <- list()
+  for(i in 1:length(P))
+  {
+    CL <- grDevices::contourLines(UD$r,z=UD$CDF,levels=P[i])
+    
+    if(length(CL)==0) # nuge sp to make a contour
+    { CL <- grDevices::contourLines(UD$r,z=UD$CDF,levels=P[i]*(1+.Machine$double.eps)) }
+    
+    # create contour heirarchy matrix (half of it)
+    H <- array(0,c(1,1)*length(CL))
+    for(row in 1:length(CL))
+    {
+      for(col in row:length(CL))
+      {
+        H[row,col] <- inside(CL[[row]],CL[[col]]) 
+      }
+    }
+    
+    # number of contours that this contour is inside
+    I <- rowSums(H)
+    
+    # if I is odd, then you are a hole inside a positive area
+    hole <- is.odd(I)
+    
+    # polygon
+    polygons[[i]] <- list()
+    for(j in 1:length(CL))
+    {
+      polygons[[i]][[j]] <- sp::Polygon( cbind( CL[[j]]$x , CL[[j]]$y ) , hole=hole[j] )
+    }
+    
+    # polygonS
+    polygons[[i]] <- sp::Polygons(polygons[[i]],ID=ID[i])
+  }
+  names(polygons) <- ID
+  
+  # spatial polygons
+  polygons <- sp::SpatialPolygons(polygons, proj4string=sp::CRS(attr(UD,"info")$projection))
+  
+  # spatial polygons data frame  
+  data <- data.frame(name=rev(ID))
+  rownames(data) <- rev(ID)
+  polygons <- sp::SpatialPolygonsDataFrame(polygons,data)
+  
+  return(polygons)
+}
+#methods::setMethod("SpatialPolygonsDataFrame",signature(Sr="UD"), function(Sr,level.UD=0.95,level=0.95) SpatialPolygonsDataFrame.UD(Sr,level.UD=level.UD,level=level))
+
+
+################
+writeShapefile.UD <- function(object, folder, file=UD@info$identity, level.UD=0.95 ,level=0.95,  ...)
+{
+  UD <- object
+  
+  SP <- SpatialPolygonsDataFrame.UD(UD,level.UD=level.UD,level=level)
+  
+  rgdal::writeOGR(SP, dsn=folder, layer=file, driver="ESRI Shapefile",...)
+}
+
+
+#########################
+# convert to spatialpoints object
+SpatialPoints.telemetry <- function(object,...)
+{
+  data <- object
+  
+  if(class(data)=="telemetry" || class(data)=="data.frame")
+  {
+    return( sp::SpatialPoints( "[.data.frame"(data,c("x","y")), proj4string=sp::CRS(attr(data,"info")$projection) ) )
+  }
+  else if(class(data)=="list")
+  {
+    SPL <- lapply( data, function(d) { sp::SpatialPoints( "[.data.frame"(d,c("x","y")), proj4string=sp::CRS(attr(d,"info")$projection) ) } )
+    return(SPL)
+  }
+}
+#methods::setMethod("SpatialPoints",signature(coords="telemetry"), function(coords) SpatialPoints.telemetry(coords))
--- R/optim.R
+++ R/optim.R
@@ -4,11 +4,12 @@
 mcoptim <- function(par,fn,method="Newton",lower=-Inf,upper=Inf,control=list(),hessian=FALSE)
 {
   method <- match.arg(method,c("Newton","Simplex"))
+  # method simplex not yet supported
   
   # check complains about visible bindings
-  fnscale <- parscale <- ndeps <- maxit <- reltol <- trace <- fast <- mc.cores <- NULL
+  fnscale <- parscale <- ndeps <- maxit <- reltol <- trace <- fast <- mc.cores <- hessian <- covariance <- NULL
   # fix default control arguments !!! add a covariance/hessian argument that takes preference over parscale
-  default <- list(fnscale=1,parscale=pmin(abs(par),abs(par-lower),abs(upper-par)),ndeps=1e-3,maxit=100,reltol=sqrt(.Machine$double.eps),trace=FALSE,fast=TRUE,mc.cores=parallel::detectCores())
+  default <- list(fnscale=1,parscale=pmin(abs(par),abs(par-lower),abs(upper-par)),ndeps=1e-3,maxit=100,reltol=sqrt(.Machine$double.eps),trace=FALSE,fast=TRUE,mc.cores=parallel::detectCores(),hessian=NULL,covariance=NULL)
   control <- replace(default,names(control),control)
   # check does not like attach
   NAMES <- names(control)
@@ -18,10 +19,9 @@ mcoptim <- function(par,fn,method="Newton",lower=-Inf,upper=Inf,control=list(),h
   
   # what we will actually be evaluating
   par <- par/parscale
-  func <- function(par) { fn(parscale*par)/fnscale }
+  func <- function(par) { fn(par*parscale)/fnscale }
   
   DIM <- length(par)
-  parscale <- array(parscale,DIM)
   lower <- array(lower,DIM)/parscale
   upper <- array(upper,DIM)/parscale
   
@@ -30,7 +30,25 @@ mcoptim <- function(par,fn,method="Newton",lower=-Inf,upper=Inf,control=list(),h
   COV <- diag(DIM) # inverse hessian
   HESS <- diag(DIM) # hesssian
   GRAD <- array(0,DIM) # gradient
-  
+
+  # do we have better information than parscale?
+  # these are all post-fnscale parameters
+  if(!is.null(covariance))
+  {
+    COV <- t(t(covariance/parscale)/parscale)
+    STD <- sqrt(diag(COV))
+    if(is.null(hessian)) { HESS <- PDsolve(COV) }
+  }
+  if(!is.null(hessian))
+  {
+    HESS <- t(t(hessian*parscale)*parscale)
+    if(is.null(covariance))
+    {
+      COV <- PDsolve(HESS)
+      STD <- sqrt(diag(COV))
+    }
+  }
+
   # start with the canonical directions
   DIR <- diag(1,DIM) # rows of column vectors
   # relative error initially assumed along directions
@@ -124,223 +142,254 @@ mcoptim <- function(par,fn,method="Newton",lower=-Inf,upper=Inf,control=list(),h
     return(list(GRAD=GRAD,HESS=HESS)) 
   }
   
+  # H <- 1 # search direciton hessian
   counts <- 0
   ######################
   # MAIN LOOP
   ######################
-  counts <- counts + 1
-  
-  ################################
-  # STEP 1: O(DIM) differentiation
-  ################################
-  # we differentiate in DIM current directions DIR from point par
-  
-  # what points are on the boundary, so that we have to do one-sided derivatives
-  LO <- (par <= lower)
-  UP <- (par >= upper)
-  BOX < UP - LO
-
-  # rotate coordinates so that BOXed dimensions are fixed/isolated in DIR and DIR[,BOX] points strictly towards boundary
-  for(i in which(BOX))
+  while(ERROR >= reltol)
   {
-    dir <- array(0,DIM)
-    dir[i] <- BOX[i]
-    DIR <- Gram.Schmidt(DIR,dir)
-  }
-  # now fix BOX to be TRUE/FALSE
-  BOX <- as.logical(BOX)
-  
-  # sample initial points surrounding the initial point for numerical differentiation
-  dP <- (ndeps*ERR)*t(STD*t(DIR))
-  P1 <- -dP + par # away from boundaries
-  P2 <- +dP + par # towards boundaries
-  # don't sample points across boundary, fold back instead - correct one-sided derivatives implemented
-  if(any(BOX)) { P2[,BOX] <- -dP[,BOX]/2 + par }
-  
-  # apply these displacements within box constraints
-  P1 <- apply(P1,2,line.boxer)
-  P2 <- apply(P2,2,line.boxer)
-  # columns are now boxed coordinates
-
-  # mc evaluate all points
-  P <- c(list(par),apply(P1,2,list),apply(P2,2,list))
-  FN <- unlist(parallelsugar::mclapply(P,func,mc.cores=mc.cores))
-  # separate back into parts
-  F1 <- FN[2:(DIM+1)]
-  F2 <- FN[(DIM+2):(1+2*DIM)]
-  fp <- FN[1] # fn(par)
-  
-  # calculate axial derivatives to second order
-  DIFF <- QuadSolve(P1,P2,DIR,F1,F2)
-  GRAD <- DIFF$GRAD
-  H <- DIFF$HESS
-  
-  # transform Hessian to current coordinates
-  HESS <- t(DIR) %*% HESS %*% DIR
-  COV <- t(DIR) %*% COV %*% DIR
-  # curvature correction factor: new curvature / old curvature
-  H <- sqrt(abs(H/diag(HESS)))
-  COR <- array(H,c(DIM,DIM))
-  COR <- t(COR) * COR
-  # update curvatures while preserving correlations
-  HESS <- COR * HESS
-  # update covariances the same way as Hessian (prevents requirement of matrix inversion)
-  COR <- array(1/H,c(DIM,DIM))
-  COR <- t(COR) * COR
-  COV <- COR * COV
-
-  # standard deviations along DIR axes
-  STD <- sqrt(abs(diag(COV)))
-  
-  # transform gradient to canonical coordinates
-  GRAD <- c(DIR %*% GRAD)
-  
-  # transform Hessian back to canonical coordinates
-  HESS <- DIR %*% HESS %*% t(DIR)
-  COV <- DIR %*% COV %*% t(DIR)
-  
-  # Newton-Raphson search step
-  dP <- -(COV %*% GRAD)
-  # don't search past boundary, but along the boundary
-  if(BOX && GRAD[BOX]>0)
-  {
-    dP[BOX] <- 0
-    dP[-BOX] <- -PDsolve(HESS[-BOX,-BOX],GRAD[-BOX])
-  }
-  
-  # test for stopping condition here
-  ERROR <- max(dP/STD)
-  if(ERROR < reltol) {} # break !!!
-  
-  # calculate best DIR and orthonormalize the remaining DIRs
-  BEST <- which.max(abs(dP)) # future index of dP direction
-  DIR <- Gram.Schmidt(DIR,dP)
-  
-  ##################
-  # LINE SEARCH LOOP
-  ##################
-  counts <- counts + 1
-  
-  # generate a linear sequence of points from old par to the other side of new par
-  P <- line.boxer(2*dP)
-  dP <- P - par
-  M <- sqrt(sum(dP^2)) # total search magnitude
-  SEQ <- seq(0,M,length.out=min(2,mc.cores)+1)[-1]
-  P <- (DIR[,BEST] %o% SEQ) + par
-
-  # evaluate objective function
-  P <- apply(P,2,list)
-  FN <- unlist(parallelsugar::mclapply(P,func,mc.cores=mc.cores))
-  
-  # pick best points including original par
-  P.ALL <- cbind(par,P)
-  F.ALL <- c(fp,FN)
-  
-  # new best estimate
-  MIN <- which.min(F.ALL)
-  par <- P.ALL[,MIN]
-  fp <- F.ALL[MIN]
-
-  # do we need to keep going to capture the minimum?
-
-  if(MIN==1) # we went too far in first step
-  {
-    # do a sub line search, including backwards reflection
+    counts <- counts + 1
     
-  }
-  
-  if(MIN==length(F.ALL)) # we didn't go far enough or we hit a boundary
-  {
-    # if we hit a boundary, then we can stop at the boundary
-    NEW.BOX <- (par <= lower) || (par >= upper)
-    if(sum(NEW.BOX)>sum(BOX)) { } # !!! goto hessian stuff
+    ################################
+    # STEP 1: O(DIM) differentiation
+    ################################
+    # we differentiate in DIM current directions DIR from point par
     
-    # Distance to first boundary that we will hit going in direction dP
-    M.BOX <- min((upper-par)[dP>0]/dP[dP>0],(lower-par)[dP<0]/dP[dP<0])
-
-    # we didn't hit a boundary yet, but we will eventually hit a boundary, so let's just do that
-    if(M.BOX<Inf)
+    # what points are on the boundary, so that we have to do one-sided derivatives
+    LO <- (par <= lower)
+    UP <- (par >= upper)
+    BOX < UP - LO
+    
+    # rotate coordinates so that BOXed dimensions are fixed/isolated in DIR and DIR[,BOX] points strictly towards boundary
+    for(i in which(BOX))
     {
-      # generate a linear sequence of points that terminate at the eventual boundary
-      SEQ <- seq(0,M.BOX,length.out=min(2,mc.cores)+1)[-1]
-      P <- (DIR[,BEST] %o% SEQ) + par
-      
-      # evaluate and take the minimum
-      P <- apply(P,2,list)
-      FN <- unlist(parallelsugar::mclapply(P,func,mc.cores=mc.cores))
-      
-      P.ALL <- cbind(par,P)
-      F.ALL <- c(fp,FN)
-      
-      # goto hessian stuff !!!
+      dir <- array(0,DIM)
+      dir[i] <- BOX[i]
+      DIR <- Gram.Schmidt(DIR,dir)
     }
+    # now fix BOX to be TRUE/FALSE
+    BOX <- as.logical(BOX)
     
-    # we didn't hit a boundary and we never will
+    # sample initial points surrounding the initial point for numerical differentiation
+    dP <- (ndeps*ERR)*t(STD*t(DIR))
+    P1 <- -dP + par # away from boundaries
+    P2 <- +dP + par # towards boundaries
+    # don't sample points across boundary, fold back instead - correct one-sided derivatives implemented
+    if(any(BOX)) { P2[,BOX] <- -dP[,BOX]/2 + par }
     
-    # generate an exponential sequence of points that terminate at the infinite boundary but drop the last point
+    # apply these displacements within box constraints
+    P1 <- apply(P1,2,line.boxer)
+    P2 <- apply(P2,2,line.boxer)
+    # columns are now boxed coordinates
     
-    # iterate this
-      
-  }
-  
-  # we improved our estimate and can stop the line search
-  
-  # take known best point
-  MIN <- which.min(F.ALL)
-  par <- P.ALL[,MIN]
-  fp <- F.ALL[MIN]
-  
-  # interpolate to an even better point
-  if(fast)
-  {
-    # take best triplet
-    if(MIN>1) # first point after boundary
-    {
-      P1 <- cbind(P[MIN-1])
-      F1 <- F.ALL[MIN-1]
-    }
-    else # first point reflected
-    {
-      P1 <- cbind(P[MIN+2])
-      F1 <- F.ALL[MIN+2]
-    }
+    # mc evaluate all points
+    P <- c(list(par),apply(P1,2,list),apply(P2,2,list))
+    FN <- unlist(parallelsugar::mclapply(P,func,mc.cores=mc.cores))
+    # separate back into parts
+    F1 <- FN[2:(DIM+1)]
+    F2 <- FN[(DIM+2):(1+2*DIM)]
+    fp <- FN[1] # fn(par)
     
-    if(MIN<length(F.ALL)) # second point before boundary
-    {
-      P2 <- cbind(P[MIN+1])
-      F2 <- F.ALL[MIN+1]
-    }
-    else # second point reflected
-    {
-      P2 <- cbind(P[MIN-2])
-      F2 <- F.ALL[MIN-2]
-    }
-    
-    # calculate gradient and curvature
-    DIFF <- QuadSolve(P1,P2,DIR[,BEST,drop=FALSE],F1,F2)
+    # calculate axial derivatives to second order
+    DIFF <- QuadSolve(P1,P2,DIR,F1,F2)
     GRAD <- DIFF$GRAD
     H <- DIFF$HESS
     
-    # estimate better location
-    par <- par - (GRAD/H)*DIR[,BEST]
+    # transform Hessian to current coordinates
+    HESS <- t(DIR) %*% HESS %*% DIR
+    COV <- t(DIR) %*% COV %*% DIR
+    # curvature correction factor: new curvature / old curvature (current coordinates)
+    H <- sqrt(abs(H/diag(HESS)))
+    COR <- array(H,c(DIM,DIM))
+    COR <- t(COR) * COR
+    # update curvatures while preserving correlations
+    HESS <- COR * HESS
+    # update covariances the same way as Hessian (prevents requirement of matrix inversion)
+    COR <- array(1/H,c(DIM,DIM))
+    COR <- t(COR) * COR
+    COV <- COR * COV
     
-    # update curvature & error along this axis?
-    # we will do better in the next iteration
+    # standard deviations along DIR axes
+    STD <- sqrt(abs(diag(COV)))
     
-    # break out of line search !!!
-  }
-  
-  
+    # transform gradient to canonical coordinates
+    GRAD <- c(DIR %*% GRAD)
+    
+    # transform Hessian back to canonical coordinates
+    HESS <- DIR %*% HESS %*% t(DIR)
+    COV <- DIR %*% COV %*% t(DIR)
+    
+    # Newton-Raphson search step
+    dP <- -(COV %*% GRAD)
+    # don't search past boundary, but along the boundary
+    if(BOX && GRAD[BOX]>0)
+    {
+      dP[BOX] <- 0
+      dP[-BOX] <- -PDsolve(HESS[-BOX,-BOX],GRAD[-BOX])
+    }
+    
+    # test for stopping condition here
+    ERROR <- max(dP/STD)
+    if(ERROR < reltol) { break } # we are finished here
+    
+    # calculate best DIR and orthonormalize the remaining DIRs
+    BEST <- which.max(abs(dP)) # future index of dP direction
+    DIR <- Gram.Schmidt(DIR,dP)
+    
+    ##################
+    # LINE SEARCH LOOP
+    ##################
+    counts <- counts + 1
+    
+    # generate a linear sequence of points from old par to the other side of new par
+    P <- line.boxer(2*dP)
+    dP <- P - par
+    M <- sqrt(sum(dP^2)) # total search magnitude
+    SEQ <- seq(0,M,length.out=min(2,mc.cores)+1)[-1]
+    P <- (DIR[,BEST] %o% SEQ) + par
+    
+    # initialize (all) storage results
+    P.ALL <- cbind(par)
+    F.ALL <- fp
+    
+    # start iteration loop
+    MIN <- 1
+    while(TRUE)
+    {
+      # most expensive part
+      # evaluate objective function at new P and store to FN
+      P <- apply(P,2,list)
+      FN <- unlist(parallelsugar::mclapply(P,func,mc.cores=mc.cores))
+      
+      # combine with older results
+      P.ALL <- cbind(P.ALL,P)
+      F.ALL <- c(F.ALL,FN)
+      
+      # sort along DIR[,BEST]
+      SORT <- colSums(P.ALL*DIR[,BEST])
+      SORT <- sort(SORT,method="quick",index.return=TRUE)$ix
+      P.ALL <- P.ALL[,SORT]
+      F.ALL <- F.ALL[SORT]
+      
+      # new best estimate
+      MIN <- which.min(F.ALL)
+      par <- P.ALL[,MIN]
+      fp <- F.ALL[MIN]
+      
+      # do we need to keep going to capture the minimum?
+      
+      if(MIN==1) # we went too far in first step
+      {
+        # do a sub line search, including backwards reflection
+        # what was the shortest step previously
+        M <- SEQ[1] * M
+        # tabulate forward steps with half of cores (assuming multiple of 2)
+        SEQ <- seq(0,M,length.out=min(1,mc.cores/2)+2)
+        SEQ <- SEQ[-c(1,length(SEQ))]
+        # tabulate backward steps other half of cores
+        SEQ <- c(rev(seq(0,-M,length.out=min(1,mc.cores/2)+1)[-1]),SEQ)
+        
+        # combine for evaluation
+        P <- (DIR[,BEST] %o% SEQ) + par
+        
+        # goto evaluate iteration step
+        next
+        
+        # if !fast, this might not be an improvement...
+        # need a conditional or fast by default
+      }
+      else if(MIN==length(F.ALL)) # we didn't go far enough or we hit a boundary
+      {
+        # if we hit a boundary, then we can stop at the boundary
+        NEW.BOX <- (par <= lower) || (par >= upper)
+        if(sum(NEW.BOX)>sum(BOX)) { break } # goto hessian stuff
+        
+        # Distance to first boundary that we will hit going in direction dP
+        M.BOX <- min((upper-par)[dP>0]/dP[dP>0],(lower-par)[dP<0]/dP[dP<0])
+        
+        # we didn't hit a boundary yet, but we will eventually hit a boundary, so let's just do that
+        if(M.BOX<Inf)
+        {
+          # generate a linear sequence of points that terminate at the eventual boundary
+          SEQ <- seq(0,M.BOX,length.out=min(2,mc.cores)+1)[-1]
+          P <- (DIR[,BEST] %o% SEQ) + par
+          
+          # goto evaluate iteration step
+          next
+        }
+        else # we didn't hit a boundary and we never will, because there is no boundary
+        {
+          # generate an exponential sequence of points that terminate at the infinite boundary but drop the last point
+          # start with a uniform sequence
+          SEQ <- seq(0,1,length.out=min(2,mc.cores)+2)
+          SEQ <- t[-c(1,length(SEQ))]
+          # log transform
+          SEQ <- M * log(1-SEQ)
+          P <- (DIR[,BEST] %o% SEQ) + par
+          
+          # update M bigly to keep accelerating next time around (if necessary)
+          M <- last(diff(SEQ)) * M
+          
+          # goto evaluate iteration step
+          next
+        }
+      }
+      else # update is safely encapsulated
+      { break }
+    }
+    # end iteration loop
+    # we improved our estimate and can stop the line search
+    
+    # interpolate to an even better point
+    if(fast)
+    {
+      # take best triplet
+      if(MIN>1) # first point after boundary
+      {
+        P1 <- cbind(P[MIN-1])
+        F1 <- F.ALL[MIN-1]
+      }
+      else # first point reflected... I don't think this can ever happen
+      {
+        P1 <- cbind(P[MIN+2])
+        F1 <- F.ALL[MIN+2]
+      }
+      
+      if(MIN<length(F.ALL)) # second point before boundary
+      {
+        P2 <- cbind(P[MIN+1])
+        F2 <- F.ALL[MIN+1]
+      }
+      else # second point reflected off boundary
+      {
+        P2 <- cbind(P[MIN-2])
+        F2 <- F.ALL[MIN-2]
+      }
+      
+      # calculate gradient and curvature
+      DIFF <- QuadSolve(P1,P2,DIR[,BEST,drop=FALSE],F1,F2)
+      GRAD <- DIFF$GRAD
+      H <- DIFF$HESS
+      # will update search direction hessian with this during next iteration
+      
+      # estimate better location if we can
+      if(H>0 && MIN<length(F.ALL)) { par <- line.boxer(par - (GRAD/H)*DIR[,BEST]) }
+    }
+    
+    # go back to differentiation
+  }  
 
-
+  if(counts==maxit) { convergence <- 1} else { convergence <- 0 }
   
   # return stuff in similar format to optim
   RETURN <- list()
-  RETURN$par <- par*parscale
+  RETURN$par <- par
   RETURN$value <- FN*fnscale
   RETURN$counts <- counts
-  RETURN$hessian <- HESS
-  RETURN$covariance <- COV
+  RETURN$convergence <- convergence
+  RETURN$hessian <- t(t(HESS/parscale)/parscale)
+  RETURN$covariance <- t(t(COV*parscale)*parscale)
   RETURN$lower <- lower
   RETURN$upper <- upper
       
