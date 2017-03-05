ced4dce81f23fc43d71f27424860c02e70b7db9b
chfleming <flemingc@si.edu>
2017-03-04 20:32:51 -0500

    stop on multiple files in zip archive

--- R/temp_unzip.R
+++ R/temp_unzip.R
@@ -10,8 +10,9 @@ temp_unzip <- function(filename, fun, ...){
   }
   temp_dir <- tempdir()
   # test if it's zip
-  f_in_zip <- try(unzip(filename, list = TRUE)$Name[1], silent = TRUE)
+  f_in_zip <- try(unzip(filename, list = TRUE)$Name, silent = TRUE)
   if (class(f_in_zip) == "character") {
+    if(length(f_in_zip)>1) { stop("Zip file contains multiple files.") }
     unzip(filename, exdir = temp_dir, overwrite = TRUE)
     dest_file <- file.path(temp_dir, f_in_zip)
   } else {
