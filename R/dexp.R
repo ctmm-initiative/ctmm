56d05c26571f2f5889bc29fb4cd79b7056ca4a45
Brock <kbrock@ab.mpg.de>
2024-06-28 14:45:57 +0200

    dexp1,dexp2 speedup

--- R/dexp.R
+++ R/dexp.R
@@ -1,5 +1,6 @@
 # numerically stable evaluation of
 # 1 - exp(-x)^2
-dexp2 <- function(x,Exp=exp(-x)) { ifelse(Exp<0.7071068,1-Exp^2,2*Exp*sinh(x)) }
+dexp2 <- function(x,Exp=exp(-x)) {if(Exp<0.7071068) return(1-Exp^2) else return(2*Exp*sinh(x))}
+
 # 1 - exp(-x)^1
-dexp1 <- function(x,Exp=exp(-x)) { ifelse(Exp<0.5,1-Exp,2*sqrt(Exp)*sinh(x/2)) }
+dexp1 <- function(x,Exp=exp(-x)) {if(Exp<0.1) return(1-Exp) else return(2*sqrt(Exp)*sinh(x/2))}
