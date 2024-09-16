1420c0bdaa7f2836ca36249b4f7105fb8b0229f1
Brock <keb266@cornell.edu>
2024-09-16 03:24:26 +0200

    Add unit tests, Speed up dexp1, dexp2, sinc, sinchw (#58)
    * Add tests
    
    * add checks for second arg of dexp1,dexp2
    
    * dexp1,dexp2 speedup
    
    * fix typo
    
    * add tests for sinch and sinc
    
    * use ifelse in sinc and sinch to improve performance

--- R/kalman.R
+++ R/kalman.R
@@ -1,9 +1,3 @@
-# numerically stable evaluation of
-# 1 - exp(-x)^2
-dexp2 <- function(x,Exp=exp(-x)) { ifelse(Exp<0.7071068,1-Exp^2,2*Exp*sinh(x)) }
-# 1 - exp(-x)^1
-dexp1 <- function(x,Exp=exp(-x)) { ifelse(Exp<0.5,1-Exp,2*sqrt(Exp)*sinh(x/2)) }
-
 ###############################
 # Propagator/Green's function and Two-time correlation from Langevin equation for Kalman filter and simulations
 # random CTMM objects need to be run through get.taus() first, to precompute various parameters
